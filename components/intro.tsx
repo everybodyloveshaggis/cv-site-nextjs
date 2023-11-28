a072113cee7762b0106a1d75637d957142ed7698
everybodyloveshaggis <everybodyloveshaggis@gmail.com>
2023-11-28 19:47:06 +0000

    about me, projects and links in intro working.

--- components/intro.tsx
+++ components/intro.tsx
@@ -11,65 +11,89 @@ export default function Intro() {
   return (
     <section className="mb-30 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center py-[-40]">
-        <div className="relative">
-          <motion.div
-            className=""
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "tween",
-              duration: 0.2,
-            }}
-          >
-            <Image
-              src="/me.JPG"
-              alt="Portrait of Scott McMahon"
-              width={152}
-              height={152}
-              priority={true}
-              className="h-28 w-28 rounded-full border-[0.35] object-cover border-white shadow-xl"
-            />
-          </motion.div>
-          <motion.span
-            className="text-4xl absolute bottom-0 right-0"
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 125,
-              delay: 0.2,
-              duration: 0.7,
-            }}
-          >
-            👋
-          </motion.span>
-        </div>
+          <div className="relative">
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                type: "tween",
+                duration: 0.2,
+              }}
+            >
+              <Image
+                src="/me.JPG"
+                alt="Portrait of Scott McMahon"
+                width={152}
+                height={152}
+                priority={true}
+                className="h-28 w-28 rounded-full border-[0.35] object-cover border-white shadow-xl"
+              />
+            </motion.div>
+            <motion.span
+              className="text-4xl absolute bottom-0 right-0"
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 125,
+                delay: 0.2,
+                duration: 0.7,
+              }}
+            >
+              👋
+            </motion.span>
+          </div>
       </div>
 
-    <motion.p className="mb-10 mt-4 px-4 text-2xl font-medium leading-[1.5] sm:text-4xl"
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }}
-    >
-        Hello, I'm{" "}<span className="font-bold text-4xl">Scott McMahon</span>
-    </motion.p>
+      >
+        Hello, I'm <span className="font-bold text-4xl">Scott McMahon</span>
+      </motion.h1>
 
-    <motion.div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
-    initial={{ opacity: 0 , y:100}}
-    animate={{ opacity: 1, y:0}}
-    transition={{
-        delay: 0.3,
-    }}
-    >
-        <Link href={"#contact"}
-        className="bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105"
-        >Contact Me{" "}
-        <BsArrowRight />
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+        }}
+      >
+        <Link
+          href={"#contact"}
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition hover:cursor-pointer"
+        >
+          Contact Me{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition " />
         </Link>
-        <a className="bg-white px-7 py-3 flex items-center gap-2 rounded-full">Download CV{" "}<BsDownload /></a>
-        <a className="bg-white p-4 flex items-center gap-2 rounded-full text-[#0077B5]"><BsLinkedin /></a>
-        <a className="bg-white p-4 flex items-center gap-2 rounded-full text-[1.20rem]"><FaGithubSquare /></a>
-    </motion.div>
 
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition hover:cursor-pointer border border-black/10"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{" "}
+          <BsDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 flex items-center gap-2 rounded-full text-[#0077B5] focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition hover:cursor-pointer border border-black/10"
+          href="https://linkedin.com"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 flex items-center gap-2 rounded-full text-[1.20rem] focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition hover:cursor-pointer border border-black/10"
+          href="https://github.com"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
