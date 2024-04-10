b84527c7d706b8010eefabbc6a78275bc17ca81a
Scott McMahon <everybodyloveshaggis@gmail.com>
2024-04-10 14:27:48 +0100

    thanks to https://github.com/stephane-monnot/react-vertical-timeline/issues/166#issuecomment-1752152276 solution timeline now works

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,18 +1,46 @@
-import React from 'react'
-import SectionHeading from './section-heading'
-import { skillsData } from '../lib/data'
+import React from 'react';
+import { skillsData } from '../lib/data';
 import { useSectionInView } from '../lib/hooks';
+import { animate, motion } from 'framer-motion'
+import SectionHeading from './section-heading';
+
+const fadeInAnimationVariance ={
+  initial: {
+    opacity: 0,
+    y:100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05*index,
+    },
+  }),
+};
 
 export default function Skills() {
+
 const { ref } =  useSectionInView('Skills');
 
   return (
-    <section ref={ref} id='skills'>
+    <section className='mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40'
+    ref={ref} 
+    id='skills'>
         <SectionHeading>My Skills</SectionHeading>
-        <ul>
+        <ul className='flex flex-wrap justify-center gap-2 text-lg text-gray-800'>
             {
                 skillsData.map((skill,index) => (
-                  <li key={index}>{skill}</li>  
+                  <motion.li className='bg-white border border-black/[0.1] rounded-xl px-5 py-3'
+                  key={index}
+                  variants={fadeInAnimationVariance}
+                  initial="initial"
+                  whileInView="animate"
+                  viewport={
+                  {
+                    once: true
+                  }
+                  }
+                  custom={index}>{skill}</motion.li>  
                 ))
             }
         </ul>
